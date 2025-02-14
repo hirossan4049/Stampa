cf44cbc734621503fa3de43e3b637d493b2d59e9
hirossan4049 <haruto405329@gmail.com>
2025-02-15 08:41:32 +0900

    Initial Commit

---
+++ Stampa/StampaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StampaApp.swift
+//  Stampa
+//
+//  Created by a on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct StampaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StampaTests/StampaTests.swift
@@ -0,0 +1,16 @@
+//
+//  StampaTests.swift
+//  StampaTests
+//
+//  Created by a on 2/15/25.
+//
+
+import Testing
+
+struct StampaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
